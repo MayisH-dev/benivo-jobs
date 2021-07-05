1de89b35205574fc74a3bab8c3c4563fd7395309
Mayis Harutyunyan <harutyunyanmayis+github@gmail.com>
2021-07-05 14:06:50 +0400

    Move generic job filtering to base spec

--- src/Benivo.Jobs.Core/JobAggregate/Specifications/PaginatedFilteredJobsSpecification.cs
+++ src/Benivo.Jobs.Core/JobAggregate/Specifications/PaginatedFilteredJobsSpecification.cs
@@ -1,36 +1,24 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Ardalis.Specification;
 
 namespace Benivo.Jobs.Core.JobAggregate.Specifications
 {
-    public sealed class PaginatedFilteredJobsSpecification : Specification<Job>
+    public sealed class PaginatedFilteredJobsSpecification : BaseFilteredJobsSpecification
     {
         public PaginatedFilteredJobsSpecification(
-            string? title,
-            IEnumerable<int>? categoryIds,
-            IEnumerable<int>? employmentTypeIds,
-            IEnumerable<int>? locationIds,
-            (int PageSize, int PageNumber)? page)
+            string? title = null,
+            IEnumerable<int>? categoryIds = null,
+            IEnumerable<int>? employmentTypeIds = null,
+            IEnumerable<int>? locationIds = null,
+            (int PageSize, int PageNumber)? page = null)
+            : base(title, categoryIds, employmentTypeIds, locationIds)
         {
             Query
                 .AsNoTracking()
                 .Include(j => j.Bookmark)
                 .Include(j => j.JobLocation)
                 .Include(j => j.EmploymentType);
-
-            if (!string.IsNullOrWhiteSpace(title))
-                Query.Where(j => j.Title.Contains(title));
-
-            if (categoryIds?.Any() ?? false)
-                Query.Where(j => categoryIds.Contains(j.Category.Id));
-
-            if (locationIds?.Any() ?? false)
-                Query.Where(j => locationIds.Contains(j.JobLocation.Id));
-
-            if (employmentTypeIds?.Any() ?? false)
-                Query.Where(j => employmentTypeIds.Contains(j.EmploymentType.Id));
 
             if (page is not null)
             {
